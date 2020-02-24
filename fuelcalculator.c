1c9044067e389a1f802eb5f9ceb301b3f588c7e7
bcooperstl <coop999@gmail.com>
2020-02-24 13:58:30 -0600

    part 2 work

--- fuelcalculator.c
+++ fuelcalculator.c
@@ -3,14 +3,17 @@
 
 long calculate_fuel(long mass)
 {
-    return (mass/3)-2;
+    long fuel = (mass/3)-2;
+    if (fuel < 0)
+        fuel = 0;
+    return fuel;
 }
 
 int main (int argc, char * argv[])
 {
     FILE * infile=NULL;
     char buffer[256];
-    long mass, fuel, total_fuel=0;
+    long mass, fuel, module_fuel, total_fuel=0;
     
     if (argc != 2)
     {
@@ -30,14 +33,22 @@ int main (int argc, char * argv[])
     while (fgets(buffer, sizeof(buffer), infile))
     {
         mass=strtol(buffer, NULL, 10);
-        fuel=calculate_fuel(mass);
-        total_fuel+=fuel;
-        fprintf(stderr, "Fuel for mass %ld is %ld\n", mass, fuel);
+        module_fuel=0;
+        do
+        {
+            fuel=calculate_fuel(mass);
+            module_fuel+=fuel;
+            fprintf(stderr, "Fuel for mass %ld is %ld\n", mass, fuel);
+            mass = fuel;
+            fprintf(stderr, "Assigning mass %ld from fuel for next calculation\n", mass);
+        } while (mass > 0);
+        fprintf(stderr, "Total fuel for this module is %ld\n", module_fuel);
+        total_fuel+=module_fuel;
     }
     
     fclose(infile);
     
-    printf("***Total Fueld is %ld***\n", total_fuel);
+    printf("***Total Fuel is %ld***\n", total_fuel);
 
     return 0;
 }
